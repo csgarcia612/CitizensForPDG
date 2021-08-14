c4137f6fe97a41d6e17f4052c6ae5e88561aa945
csgarcia612 <csgarcia612@comcast.net>
2021-08-14 17:59:21 -0500

    updated packages and fixed proxy-middleware error

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,5 +1,11 @@
-const proxy = require('http-proxy-middleware');
+// const proxy = require('http-proxy-middleware');
 
-module.exports = app => {
-  app.use('/api', proxy({ target: 'http://localhost:4000' }));
+// module.exports = app => {
+//   app.use('/api', proxy({ target: 'http://localhost:4000' }));
+// };
+
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = (app) => {
+  app.use('/api', createProxyMiddleware({ target: 'http://localhost:4000' }));
 };
